55b93cf440add5ba45057d0f0ea11e87f1b6b931
samuel-2018 <45561451+samuel-2018@users.noreply.github.com>
2019-07-21 17:49:28 -0400

    added photo info overlay

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -3,7 +3,27 @@ import React from "react";
 const Photo = props => {
   return (
     <li>
-      <img src={props.imageData.urls.small} alt={props.imageData.description} />
+      <figure>
+        <img
+          src={props.imageData.urls.small}
+          alt={props.imageData.description}
+        />
+
+        {/* Creates a layer for inset shadow.*/}
+        <div className="shadow-inset" />
+
+        <figcaption>
+          <a className="img-link" href={props.imageData.urls.full}>
+            Photo
+          </a>{" "}
+          by{" "}
+          <a className="img-link" href={props.imageData.user.links.html}>
+            {props.imageData.user.name}
+          </a>{" "}
+          on Unsplash
+        </figcaption>
+      </figure>
+
       <div />
     </li>
   );
